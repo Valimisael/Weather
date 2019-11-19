090b0f3d4eeefbbd2f13e6b385f3311244dd187e
Valeryia Himzheuskaya <valimisael@yahoo.com>
2019-11-19 23:53:10 +0300

    Update API key and values

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(
     function () {
 
         function showCurrentWeather() {
-            getCurrentWeather('http://api.apixu.com/v1/current.json?key=03dc943cd1e746a2be8112030181004&q=Minsk')
+            getCurrentWeather('http://api.weatherstack.com/current?access_key=2a18cdef8ccb209bdc8e0a4c414138fe&query=California')
                 .done(function (data) {
                     var weather = data;
 
@@ -10,16 +10,16 @@ $(document).ready(
                         country = weather.location.country,
                         time = weather.location.localtime,
 
-                        condition = weather.current.condition.text,
-                        icon = 'http:' + weather.current.condition.icon,
+                        condition = weather.current.weather_descriptions.text,
+                        icon = "" + weather.current.weather_icons,
 
-                        temp = weather.current.temp_c + ' °C',
+                        temp = weather.current.temperature + ' °C',
 
-                        wind = weather.current.wind_mph + ' m/s',
+                        wind = weather.current.wind_speed + ' m/s',
 
                         humidity = weather.current.humidity + ' %',
 
-                        vision = weather.current.vis_km + ' km';
+                        vision = weather.current.visibility + ' km';
 
                     var weatherWidget = $(document.createElement('div')).addClass('weatherWidget'),
                         widgetBlock = $(document.createElement('div')).addClass('widgetBlock');
